aa6e7b63f31892153542d164721ab2659c813bbc
Musyahid Abror <musyahid2@gmail.com>
2020-08-20 11:26:38 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,128 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="ALBUMS APPS"/>
+    <div class="font-sans bg-grey-lighter flex flex-col min-h-screen w-full">
+      <div class="flex-grow container mx-auto sm:px-4 pt-6 ">
+        <div class="bg-white border-t border-b sm:border-l sm:border-r sm:rounded shadow mb-6">
+          <div class="hidden lg:flex">
+            <div class="w-1/2 text-center py-8">
+                  <!-- This is an example component -->
+              <div class="pt-2 relative mx-auto text-gray-600">
+                <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                  type="text" name="todo" id="todo" placeholder="Todo..." v-model="todo" @keyup.enter="addTodo">
+                  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" @click="addTodo()">
+                    <span>ADD TODO</span>
+                  </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="grid grid-flow-col grid-cols-3 grid-rows-3 gap-4">
+          <div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <img class="w-full" src="https://via.placeholder.com/600/92c952" alt="Sunset in the mountains">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">sds</div>
+              </div>
+              <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <img class="w-full" src="https://via.placeholder.com/600/92c952" alt="Sunset in the mountains">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">sds</div>
+              </div>
+              <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <img class="w-full" src="https://via.placeholder.com/600/92c952" alt="Sunset in the mountains">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">sds</div>
+              </div>
+              <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <img class="w-full" src="https://via.placeholder.com/600/92c952" alt="Sunset in the mountains">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">sds</div>
+              </div>
+              <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <img class="w-full" src="https://via.placeholder.com/600/92c952" alt="Sunset in the mountains">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">sds</div>
+              </div>
+              <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              </div>
+            </div>
+          </div>
+          <div v-for="album of albums" :key="album.id">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+              <img class="w-full" src="https://via.placeholder.com/600/92c952" alt="Sunset in the mountains">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">{{album.title}}</div>
+              </div>
+              <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      albums: []
+    }
+  },
+    async created() {
+    try {
+        const res = await axios.get(`http://localhost:3000/albums`);
+        this.albums = res.data;
+    } catch (e) {
+        console.log(e)
+    }
   }
 }
 </script>
